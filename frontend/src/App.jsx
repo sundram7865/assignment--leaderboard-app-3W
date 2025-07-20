f0a2e379d57367bb9cd91b6dc8280d0cda7954f9
sundram <ram322222@gmail.com>
2025-07-20 08:28:53 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl'>TEXT3-XL</div>
+  )
+}
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-white shadow p-4">
+      <h1 className="text-2xl font-bold text-center text-indigo-600">🏆 3W Leaderboard</h1>
+    </header>
+  )
+}
