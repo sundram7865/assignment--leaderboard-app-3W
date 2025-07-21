ea46897f59c07b31170ecde0b22324874e74ed0c
sundram <ram322222@gmail.com>
2025-07-21 11:02:28 +0530

    configured for deployment

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: FRONTEND_URL, // Your frontend URL
   credentials: true
 }));
 app.use(express.json());
